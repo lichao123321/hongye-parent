e4089c0b055de82eae594fa68202885ced5644ed
lichao <15045968292@163.com>
2021-02-22 09:21:29 +0800

    生成器连接配置修改

--- hongye-web/src/main/java/com/hongye/config/MpGenerator.java
+++ hongye-web/src/main/java/com/hongye/config/MpGenerator.java
@@ -54,10 +54,10 @@ public class MpGenerator {
                 return super.processTypeConvert(fieldType);
             }
         });
-        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("root");
-        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/hongye?characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/hongye?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT");
         mpg.setDataSource(dsc);
 
         // 策略配置
@@ -65,7 +65,7 @@ public class MpGenerator {
         // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
         strategy.setTablePrefix(new String[] { "tb_"});// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-         strategy.setInclude(new String[] { "tb_notice" }); // 需要生成的表
+         strategy.setInclude(new String[] { "tb_test" }); // 需要生成的表
         // strategy.setExclude(new String[]{"test"}); // 排除生成的表
         // 自定义实体父类
 //         strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
